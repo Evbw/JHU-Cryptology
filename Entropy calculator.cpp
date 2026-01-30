bdda8eb94d43164f280c162761da7d374c3ec42f
Everett <everett.bowline@gmail.com>
2026-01-30 08:54:17 -0500

    Added programmatic comment and constant factors

--- Entropy calculator.cpp
+++ Entropy calculator.cpp
@@ -1,6 +1,15 @@
+/*
+    File name: Entropy calculator
+    Purpose: Simple program for calculating the entropy of fixed probabilities and keys
+*/
+
 #include <iostream>
 using namespace std;
 
+//There is a fixed number of plaintext elements, keys, and ciphertexts
+const int NUMP = 3, NUMK = 3, NUMC = 4;
+
 int main() {
+    
     
 }
