56e407127f58619e9fded26983c82a0692d31ef9
Everett <everett.bowline@gmail.com>
2026-01-30 09:16:35 -0500

    Began calculating the values of P(c), realized I needed different variable names, so updated p->pV and k->kV

--- Entropy calculator.cpp
+++ Entropy calculator.cpp
@@ -12,14 +12,20 @@ const int NUMP = 3, NUMK = 3, NUMC = 4;
 
 int main() {
     //Values for a, b, and c stored in a vector
-    vector<double> p = { 1.0/6.0, 2.0/6.0, 3.0/6.0 };
+    vector<double> pV = { 1.0/6.0, 2.0/6.0, 3.0/6.0 };
     //Values for keys, which are equiprobably, in a vector
-    vector<double> k = { 1.0/3.0 };
+    vector<double> kV = { 1.0/3.0 };
 
     //Encryption matrix e[k][p] = c (with 0, 1, 2, 3 corresponding to ciphertext 1, 2, 3, 4)
     int e[NUMK][NUMP] {
-        {0, 1, 2}
-        {1, 2, 3}
+        {0, 1, 2},
+        {1, 2, 3},
         {2, 3, 0}
+    };
+
+    //Compute value for P(C)
+    vector<double> cV(NUMC, 0.0);
+    for ( int k = 0; k < NUMK; k++ ) {
+        
     }
 }
