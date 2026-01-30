5299a362836e2068c6fda8de1c238abc11ffd6b5
Everett <everett.bowline@gmail.com>
2026-01-30 09:01:15 -0500

    Added vector library and vectors for values of plaintext and keys

--- Entropy calculator.cpp
+++ Entropy calculator.cpp
@@ -4,12 +4,16 @@
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 //There is a fixed number of plaintext elements, keys, and ciphertexts
 const int NUMP = 3, NUMK = 3, NUMC = 4;
 
 int main() {
-    
-    
+    //Values for a, b, and c stored in a vector
+    vector<double> p = { 1.0/6.0, 2.0/6.0, 3.0/6.0 };
+    //Values for keys, which are equiprobably, in a vector
+    vector<double> k = { 1.0/3.0 };
+
 }
