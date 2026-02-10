85fe4da342664e6b7c8e865203a0dcef2d5eae67
Everett <everett.bowline@gmail.com>
2026-02-10 10:11:39 -0500

    Added xord function to check the values of the dot product

--- Linear S-box.cpp
+++ Linear S-box.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int xord(int mask, int val) {
+    int bits = mask & val;
+    int parity = 0;
+    while (bits) {
+        parity ^= (bits & 1);
+        bits >>= 1;
+    }
+    return parity;
+}
+
 int main() {
     int sbox[16] = {0x7, 0x3, 0xA, 0x5, 0x4, 0x8, 0x9, 0x2, 0xE, 0x6, 0xC, 0x1, 0xD, 0xB, 0x0, 0xF};
 
@@ -9,7 +19,7 @@ int main() {
             int count = 0;
             for ( int x = 0; x < 16; x++ ) {
                 int y = sbox[x];
-                
+                int ax = xord(a, x);
             }
         }
     }
