b1909e814e28128922ed466227597b3e7ecf8071
Everett <everett.bowline@gmail.com>
2026-03-01 15:15:13 -0500

    Added cstdint library and started initial logic for search function

--- Proof of Work.cpp
+++ Proof of Work.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <cstdint>
 #include <openssl/evp.h>    //OpenSSL's envelope hashing API
 using namespace std;
 
 const int CHECKEDBYTES = 3;
 
 void search(char* openssl_name, int input_bytes, int leading_zero_bytes) {
-    
+
+    uint64_t total_inputs = 1ULL << (input_bytes*8);    //For 3 bytes, 2^24 = 16777216 and needed an unsigned long long to handle a 64-bit number
+
+    int count = 0;
+    uint8_t buffer[8] = {};                             //Buffer to hold input bytes
+
+    for ( uint64_t i = 0; i < total_inputs; i++ ) {
+
+        for ( int b = 0; b < input_bytes; b++ ) {
+            int shift_amount = (input_bytes - 1 - b) * 8;
+            buffer[b] = static_cast<uint8_t>(i >> shift_amount) & 0xFF;
+        }
+    }
 }
 
 int main() {
