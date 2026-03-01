3ea75af489b0928dcf77b6862c2dda24c2220144
Everett <everett.bowline@gmail.com>
2026-03-01 14:06:18 -0500

    Added OpenSSL library and added search function

--- Proof of Work.cpp
+++ Proof of Work.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <openssl/evp.h>    //OpenSSL's envelope hashing API
 using namespace std;
 
 const int CHECKEDBYTES = 3;
+
+void search(char* openssl_name, int input_bytes, int leading_zero_bytes) {
+    
+}
 
 int main() {
 
     int input_bytes = CHECKEDBYTES;
     int leading_zero_bytes = CHECKEDBYTES;
+
+    cout<<"Searching all "<<input_bytes<<"-byte strings for digests starting with "<<leading_zero_bytes<<" zero bytes";
+
+    search("SHA1", input_bytes, leading_zero_bytes);
+    search("SHA512", input_bytes, leading_zero_bytes);
+    search("SHA3-512", input_bytes, leading_zero_bytes);
 
     return 0;
 }
