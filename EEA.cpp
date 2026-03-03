9a7dfc8cf3070f0f83abcb97d40423b63769c4cf
Everett <everett.bowline@gmail.com>
2026-03-03 15:54:27 -0500

    Added variable input and cmath library

--- EEA.cpp
+++ EEA.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     
+    long long a, b, x;
+    cout<<"Enter a value for a: "<<endl;
+    cin>>a;
+    cout<<"Enter an exponent (a value of 0 will be disregarded and behave as a value of 1): "<<endl;
+    cin>>x;
+    if ( x == 0 ) {
+        x = 1;
+    }
+    a = pow(a, x);
+    cout<<"Enter a value for b: "<<endl;
+    cin>>b;
+
+        
 }
