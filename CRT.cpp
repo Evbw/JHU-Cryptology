c387c04c1b2ccca92b6f1e6727b7e6b117e02c4c
Everett <everett.bowline@gmail.com>
2026-03-03 17:59:01 -0500

    Initial commit (and added variables)

---
+++ CRT.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    vector<long long> a = {29683, 14495, 136776};
+    vector<long long> m = {163659, 146921, 193331};
+
+    cout<<"Enter a value for a: "<<endl;
+    cin>>a;
+    cout<<"Enter a value for modulo m: "<<endl;
+    cin>>m;
+}
