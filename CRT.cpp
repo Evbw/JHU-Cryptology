9b6825234074fc69e6fbd3364b23c4e543875dce
Everett <everett.bowline@gmail.com>
2026-03-03 17:59:28 -0500

    Added vector library

--- CRT.cpp
+++ CRT.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     vector<long long> a = {29683, 14495, 136776};
     vector<long long> m = {163659, 146921, 193331};
 
-    cout<<"Enter a value for a: "<<endl;
-    cin>>a;
-    cout<<"Enter a value for modulo m: "<<endl;
-    cin>>m;
 }
