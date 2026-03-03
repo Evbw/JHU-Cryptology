995f0417e98f42b2e804b6f0eab51bd2a0a7d0e1
Everett <everett.bowline@gmail.com>
2026-03-03 17:08:36 -0500

    Finished sam logic

--- SAM.cpp
+++ SAM.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-unsigned long long sam(unsigned long long x, unsigned long long c, unsigned long long n) {
-    
+unsigned long long sam(unsigned long long x, unsigned long long c, unsigned long long n, unsigned long long z) {
+    vector<int> bits;
+    unsigned long long temp = c;
+
+    while ( temp > 0 ) {
+        bits.push_back(temp % 2);
+        temp /= 2;
+    }
+
+    int l = bits.size();
+
+    for ( int i = l - 1; i >= 0; i-- ) {
+        z = z * z % n;
+        if ( bits[i] == 1 ) {
+            z = z * x % n;
+        }
+    }
+
+    return z;
 }
 
 int main() {
@@ -15,5 +33,5 @@ int main() {
     cout<<"Enter a value for mod n: "<<endl;
     cin>>n;
 
-    z = sam(x, c, n);
+    z = sam(x, c, n, z);
 }
