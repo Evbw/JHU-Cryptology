0166dee83674cf1be149f74987e58983231713f9
Everett <everett.bowline@gmail.com>
2026-03-03 16:51:56 -0500

    Added variable input

--- SAM.cpp
+++ SAM.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
-    
+    unsigned long long x, c, n, z = 1;
+
+    cout<<"Enter a value for base c: "<<endl;
+    cin>>c;
+    cout<<"Enter a value for an exponent x: "<<endl;
+    cin>>x;
+    cout<<"Enter a value for mod n: "<<endl;
+    cin>>n;
 }
