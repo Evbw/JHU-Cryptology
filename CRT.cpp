04a89b3bdccbdd36276b83db2905df283e901b84
Everett <everett.bowline@gmail.com>
2026-03-03 19:22:29 -0500

    Finished mod_inverse function logic

--- CRT.cpp
+++ CRT.cpp
@@ -3,7 +3,30 @@
 using namespace std;
 
 long long mod_inverse(long long a, long long b) {
+    long long a0 = a;
+    long long b0 = b;
+    long long t0 = 0;
+    long long t = 1;
+    long long q = a0/b0;
+    long long r = a0-q*b0;
+    long long temp;
+
+    while ( r > 0 ) {
+        temp = ((t0-q*t)%a+a);
+        t0 = t;
+        t = temp;
+        a0 = b0;
+        b0 = r;
+        q = a0/b0;
+        r = a0-q*b0;
+    }
+
+    if ( b0 != 1 ) {
+        cout<<b<<" has no irreversible modulo "<<a<<"."endl;
+        return -1;
+    }
     
+    return t;
 }
 
 int main() {
@@ -19,10 +42,18 @@ int main() {
 
     __int128 x = 0;
     __int128 Mi = 0;
+    __int128 term = 0;
     long long yi = 0;
     for ( int i = 0; i < k; i++ ) {
         Mi = M/m[i];
-        yi = mod_inverse(m[i], (long long)(Mi % m[i]));
+        yi = mod_inverse(m[i], static_cast<long long>(Mi % m[i]));
+        term = static_cast<__int128>a[i] * (Mi % M);
+        term %= M;
+        term *= yi;
+        term %= M;
+        x = (x + term) % M;
     }
 
+
+    return 0;
 }
