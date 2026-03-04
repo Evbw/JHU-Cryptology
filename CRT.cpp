1de1f9abd02be70e8d3a96814b0d3f3b11d33063
Everett <everett.bowline@gmail.com>
2026-03-03 19:04:20 -0500

    Added initial logic in main and mod_inverse function

--- CRT.cpp
+++ CRT.cpp
@@ -2,8 +2,27 @@
 #include <vector>
 using namespace std;
 
+long long mod_inverse(long long a, long long b) {
+    
+}
+
 int main() {
     vector<long long> a = {29683, 14495, 136776};
     vector<long long> m = {163659, 146921, 193331};
+
+    int k = a.size();
+
+    __int128 M = 1;
+    for ( int i = 0; i < k; i++ ) {
+        M *= m[i];
+    }
+
+    __int128 x = 0;
+    __int128 Mi = 0;
+    long long yi = 0;
+    for ( int i = 0; i < k; i++ ) {
+        Mi = M/m[i];
+        yi = mod_inverse(m[i], (long long)(Mi % m[i]));
+    }
 
 }
