5dcb6e9724106616b7fbc1f65e8510bbcc5045e3
Everett <everett.bowline@gmail.com>
2026-03-10 15:07:56 -0400

    Added vector library

--- DixonsRandomSquares.cpp
+++ DixonsRandomSquares.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     
+    int n = 347881;
+
+    int base[] = {-1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
+    vector<int> smooth_z;
+    vector<int> smooth_vals;
+    vector<vector<int>> smooth_exponents;
 }
