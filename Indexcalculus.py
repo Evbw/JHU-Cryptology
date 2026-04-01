ad1fca251d2f6a5c447f5c9a85c9065a17477f4e
Everett <everett.bowline@gmail.com>
2026-04-01 11:36:23 -0400

    Began part a calculations and defined factor_over_base function

--- Indexcalculus.py
+++ Indexcalculus.py
@@ -1,10 +1,22 @@
 import math
 
+def factor_over_base(value, base):
+
+
 def main():
 
     p = 227
     n = p-1
     alpha = 2                       #Order of Z_p^* = 226
+
+    exponents = [32, 40, 59, 156]   #Given exponents
+    relations = []                  #will hold (exponent, [2, 3, 5, 7, 11])
+
+    factor_base = [2, 3, 5, 7, 11]
+
+    for k in exponents:
+        val = pow(alpha, k, p)
+        exps = factor_over_base(val, factor_base)
 
 if __name__ == "__main__":
     main()
