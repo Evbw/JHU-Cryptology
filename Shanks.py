84960845cad90d6a78146885f31eedbaec6ad2d8
Everett <everett.bowline@gmail.com>
2026-04-01 10:26:56 -0400

    Added hardcoded values for shanks function

--- Shanks.py
+++ Shanks.py
@@ -1,6 +1,20 @@
-
+import math
 
 def main():
+
+    p1 = 24691
+    n = p1-1
+    alpha = 106
+    beta = 12375
+    x1 = shanks(p1, n, alpha, beta)
+    print(x1)
+
+    p2 = 458009
+    n = p2-1
+    alpha = 6
+    beta = 248338
+    x2 = shanks(p2, n, alpha, beta)
+    print(x2)
 
 if __name__ == "__main__":
     main()
