4c3aa02719ea47388b743896efac71d6223ad909
Everett <everett.bowline@gmail.com>
2026-04-01 11:08:31 -0400

    Added shanks function

--- Shanks.py
+++ Shanks.py
@@ -1,5 +1,29 @@
 import math
 
+def shanks(p, n, alpha, beta):
+
+    m = math.isqrt(n)             #Set the square root of n
+
+    if m*m < n:
+        m += 1
+
+    alpha_m = pow(alpha, m, p)      #Start the baby steps. a^m mod p
+    L1 = {}                         #Dictionary to store the list
+    power = 1                       #a^(m*0) = 1
+    for j in range(m):
+        L1[power] = j
+        power = (power*alpha_m) % p
+
+    alpha_inv = pow(alpha, -1, p)   #Start the giant steps. a^(-1) mod p
+    gamma = beta % p                #i = 0: B*a^0
+    for i in range(m):
+        if gamma in L1:             #Find a matching set of coordinates
+            j = L1[gamma]
+            return (m*j+i) % n      #log_a(B) = (m*j+i) mod n
+        gamma = (gamma*alpha_inv) % p
+    
+    return None
+
 def main():
 
     p1 = 24691
