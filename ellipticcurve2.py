532754dba5ab3a1306a57fce9165019507c5eb5d
Everett <everett.bowline@gmail.com>
2026-04-06 14:36:18 -0400

    Added some logic to main and declared point_add function

--- ellipticcurve2.py
+++ ellipticcurve2.py
@@ -1,6 +1,11 @@
 import math
 
-
+def point_add(P, Q, a, p):
+    x1 = P[0]
+    y1 = P[1]
+    x2 = Q[0]
+    y2 = Q[1]
+    lam = ((y2 - y1) * modinv(x2 - x1, p)) % p
 
 def main():
     a = 1
@@ -11,7 +16,8 @@ def main():
     Pb = (11, 15)
     Pc = (4, 27)
 
-    
+    A = point_add(Pa1, Pa2, a, p)
+    print(f"Addition: {A}")
 
 if __name__ == "__main__":
     main()
