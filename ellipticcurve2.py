5579bfd9748c604f63d12b35f4580bdb4071ad91
Everett <everett.bowline@gmail.com>
2026-04-06 14:30:39 -0400

    Got rid of unnecessary code and declared points as tuples

--- ellipticcurve2.py
+++ ellipticcurve2.py
@@ -1,45 +1,17 @@
 import math
 
-def isNonSingular(a, b, p):
-    return 0 != (( 4*a*a*a + 27*b*b) % p)
 
-def f(x, a, b, p):
-    return (x*x*x + a*x + b) % p
 
 def main():
     a = 1
-    b = 28
-    p = 71
-    quadres = set()
-    counter = 1
+    b = 26
+    p = 127
+    Pa1 = (9, 16)
+    Pa2 = (6, 11)
+    Pb = (11, 15)
+    Pc = (4, 27)
 
-    if not isNonSingular(a, b, p):
-        print("Invalid value")
-        return 0
     
-    for x in range(1,p):
-        qr = (x*x) % p
-        quadres.add(qr)
-
-    for quad in quadres:
-        print(f"{counter}: {quad}")
-        counter+=1
-
-    print( (p%4) == 3)
-    testexp = (p-1)//2
-    rootexp = (p+1)//4
-    print("Test exponent: ", testexp)
-    print("Root exponent: ", rootexp)
-
-    for x in range(0,p):
-        r = f(x, a, b, p)
-        if pow(r,testexp,p) == 1:
-            y1 = pow(r,rootexp,p)
-            y2 = (-y1) % p
-            print(f"({x}, {y1})")
-            print(f"({x}, {y2})")
-        if r == 0:
-            print(f"({x}, 0)")
 
 if __name__ == "__main__":
     main()
