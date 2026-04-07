d1d63be17ba9973c1daf82aa533f7e1942f503c2
Everett <everett.bowline@gmail.com>
2026-04-07 14:28:44 -0400

    Removed extraneous functions

--- ellipticcurve.py
+++ ellipticcurve.py
@@ -1,8 +1,5 @@
 import math
 
-def isNonSingular(a, b, p):
-    return 0 != (( 4*a*a*a + 27*b*b) % p)
-
 def f(x, a, b, p):
     return (x*x*x + a*x + b) % p
 
@@ -10,26 +7,9 @@ def main():
     a = 1
     b = 28
     p = 71
-    quadres = set()
-    counter = 1
 
-    if not isNonSingular(a, b, p):
-        print("Invalid value")
-        return 0
-    
-    for x in range(1,p):
-        qr = (x*x) % p
-        quadres.add(qr)
-
-    for quad in quadres:
-        print(f"{counter}: {quad}")
-        counter+=1
-
-    print( (p%4) == 3)
     testexp = (p-1)//2
     rootexp = (p+1)//4
-    print("Test exponent: ", testexp)
-    print("Root exponent: ", rootexp)
 
     for x in range(0,p):
         r = f(x, a, b, p)
